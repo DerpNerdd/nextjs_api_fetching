b3c403f1f7138b72ecf24555a9772d2684436f85
DerpNerd <sanchez1.alan1@gmail.com>
2025-02-27 19:46:34 -0700

    Bonus points api routing

---
+++ nextjs_api/app/api/users/route.js
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const users = await response.json();
+    // Return data as JSON
+    return NextResponse.json(users);
+  } catch (error) {
+    return NextResponse.json(
+      { error: error.message || 'Something went wrong' },
+      { status: 500 }
+    );
+  }
+}
